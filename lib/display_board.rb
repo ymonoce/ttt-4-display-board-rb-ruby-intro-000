45157ca9121bc521cac89a8dbaf3e06717d41a1f
Chloe Atwood <atwoodksw@gmail.com>
2020-10-17 02:29:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,13 @@
 #tic tac toe board display
 
 # define array with nine empty strings
-board = ["","","","","","","","",""]
+board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 # define method with arguement
 def display_board(board)
-  puts "#{board[0]}   |#{board [1]}   |#{board[2]}   " # puts row 1 of board
+  puts " #{board[0]}  | #{board [1]}  | #{board[2]}  " # puts row 1 of board
   puts "-----------" # puts border of board
-  puts "#{board[3]}   |#{board[4]}   |#{board[5]}   " # puts row 2 of board
+  puts " #{board[3]}  | #{board[4]}  | #{board[5]}  " # puts row 2 of board
   puts "-----------" # puts border of board border
-  puts "#{board[6]}   |#{board[7]}   |#{board[8]}   " # puts row 3 of board
+  puts " #{board[6]}  | #{board[7]}  | #{board[8]}  " # puts row 3 of board
 # end definition of method ln. 6
 end
