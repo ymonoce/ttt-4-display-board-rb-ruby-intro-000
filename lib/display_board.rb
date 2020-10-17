72be666ed53f7ed44420ebae2ca3384bb5164e60
Chloe Atwood <atwoodksw@gmail.com>
2020-10-17 02:23:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# define method with arguement
+def display_board(board)
+  puts "#{board[0]}   |#{board [1]}   |#{board[2]}   " # row 1
+  puts "-----------" # border
+  puts "#{board[3]}   |#{board[4]}   |#{board[5]}   " # row 2
+  puts "-----------" # border
+  puts "#{board[6]}   |#{board[7]}   |#{board[8]}   " # row 3
+end
