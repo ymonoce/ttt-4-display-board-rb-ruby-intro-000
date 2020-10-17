d2dedfb8d40db6fdcd13aa41c86015f30b14d3f2
Chloe Atwood <atwoodksw@gmail.com>
2020-10-17 02:30:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,5 +9,5 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} " # puts row 2 of board
   puts "-----------" # puts border of board border
   puts " #{board[6]} | #{board[7]} | #{board[8]} " # puts row 3 of board
-# end definition of method ln. 6
+# end definition of method ln. six
 end
